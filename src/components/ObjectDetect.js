937fa2eaedd23850f14579137623a8c3d0df5008
thairyo <sonthai1310.works@gmail.com>
2022-07-30 16:47:20 +0700

    Start building & Using the File Picker to load images.

---
+++ src/components/ObjectDetect.js
@@ -0,0 +1,87 @@
+import React, { useRef } from "react";
+import styled from "styled-components";
+
+import "@tensorflow/tfjs-backend-cpu";
+import "@tensorflow/tfjs-backend-webgl";
+import * as cocoSsd from "@tensorflow-models/coco-ssd";
+
+const ObjectDetectorContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const DetectorContainer = styled.div`
+  min-width: 200px;
+  height: 700px;
+  border: 3px solid #fff;
+  border-radius: 5px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+`;
+
+const TargetImg = styled.img`
+  height: 100%;
+`;
+
+const HiddenFileInput = styled.input`
+  display: none;
+`;
+
+const SelectButton = styled.button`
+  padding: 7px 10px;
+  border: 2px solid transparent;
+  background-color: #fff;
+  color: #0a0f22;
+  font-size: 16px;
+  font-weight: 500;
+  outline: none;
+  margin-top: 2em;
+  cursor: pointer;
+  transition: all 260ms ease-in-out;
+  &:hover {
+    background-color: transparent;
+    border: 2px solid #fff;
+    color: #fff;
+  }
+`;
+
+const TargetBox = styled.div`
+  position: absolute;
+  left: ${({ x }) => x + "px"};
+  top: ${({ y }) => y + "px"};
+  width: ${({ width }) => width + "px"};
+  height: ${({ height }) => height + "px"};
+  border: 4px solid #1ac71a;
+  background-color: transparent;
+  z-index: 20;
+  &::before {
+    content: "${({ classType, score }) => `${classType} ${score.toFixed(1)}%`}";
+    color: #1ac71a;
+    font-weight: 500;
+    font-size: 17px;
+    position: absolute;
+    top: -1.5em;
+    left: -5px;
+  }
+`;
+
+const ObjectDetect = (props) => {
+  const fileInputRef = useRef();
+
+  const openFilePicker = () => {
+    if (fileInputRef.current) fileInputRef.current.click();
+  };
+
+  return (
+    <ObjectDetectorContainer>
+      <DetectorContainer>Img</DetectorContainer>
+      <HiddenFileInput type="file" ref={fileInputRef} />
+      <SelectButton onClick={openFilePicker}>Select Image</SelectButton>
+    </ObjectDetectorContainer>
+  );
+};
+
+export default ObjectDetect;
